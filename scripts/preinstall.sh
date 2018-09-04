3ebf93acb0704d7a8395496b56045a2026451659
Lucas Domáradzkí <33641962+lucasDomaradzki@users.noreply.github.com>
2018-09-04 20:05:42 -0300

    Docker preinstall script (#2)
    Removed docker and improved the preinstall script

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+sudo rm -rf ./config/workspace.json
 #Check if the workspace.json is defined, if not it creates it
 if [[ ! -e "config/workspace.json" ]];then
   touch 'config/workspace.json'
@@ -12,22 +14,10 @@ if [[ ! -e "config/workspace.json" ]];then
   done
   if [ "$input1" != "" ] && [ "$input2" != "" ];then
     echo '{"workspace": "'$input1'", "mongoDB": "'$input2'"}' > "config/workspace.json"
-    
+    WORKSPACE=$(jq '.workspace' -r config/workspace.json)
+    MONGODB="$(jq '.mongoDB' -r config/workspace.json)"
+    sudo npm install --global gulp
+    sudo npm install gulp-cli -g
+    sudo mkdir -p $MONGODB/data/db
   fi
 fi
-
-#Check if container projetomaria exists, if not it loads Dockerfile and creates it
-if [ ! $(docker ps -aq --filter name=projetomaria) ];then
-  WORKSPACE=$(jq '.workspace' -r config/workspace.json)
-  MONGODB="$(jq '.mongoDB' -r config/workspace.json)"
-  sudo npm install --global gulp
-  sudo npm install gulp-cli -g
-  sudo mkdir -p $MONGODB/data/db
-  sudo usermod -a -G docker $USER
-  docker pull ubuntu:16.04
-  docker build --no-cache -t projetomaria .
-  docker run --name=projetomaria -d -v $WORKSPACE:/workspace -p 8080:80 projetomaria
-  DOCKER_IP=$(docker inspect $(docker ps -aq --filter "name=projetomaria") | jq -r .[0].NetworkSettings.IPAddress)
-  sudo sh -c "echo $DOCKER_IP local.projetomaria.com.br >> /etc/hosts"
-fi
-
