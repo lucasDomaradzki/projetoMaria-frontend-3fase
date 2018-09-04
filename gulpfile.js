0292fcb3fc6e8a40c23a128ffcb25f8746f13026
lucasDomaradzki <lucas_domaradzki@estudante.sc.senai.br>
2018-09-04 20:00:58 -0300

    Removed docker and improved the preinstall script

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,8 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('run:bash', shell.task('docker exec -it $(docker ps -aq --filter name=projetomaria) bash'));
+// To do
+gulp.task('run:mongo', shell.task(''));
 
-gulp.task('start:maria', shell.task('docker start $(docker ps -aq --filter name=projetomaria)'));
-
-gulp.task('stop:maria', shell.task('docker stop $(docker ps -aq --filter name=projetomaria)'));
-
-gulp.task('status:maria', shell.task('docker ps -a --filter name=projetomaria'));
-
-var mongo = gulp.task('get:mongo', shell.task('cat config/workspace.json | jq ".mongoDB" -r'));
-
-gulp.task('run:server', function() {
-  gulp.src(mongo)
-    .pipe(shell.task('cat config/workspace.json | jq ".mongoDB" -r'))
-    .pipe(shell.task(''))
-});
+// To do
+gulp.task('', shell.task(''));
