8a1d9a7420ddeab7d1fae724e4ca89682f132987
Daniel Prando <danielcprando@hotmail.com>
2018-10-11 20:51:08 -0300

    rotas do crud, download de arquivo, parseCSVtoJSON, estrutura de pastas

--- src/service/functions/makingCall.ts
+++ src/service/functions/makingCall.ts
@@ -1,39 +1,28 @@
 import { CallApi } from './callApi';
-import { UTIL } from '../../messages';
+import { UTIL } from './messages';
 
-export class MakingCall {
+export default class RedirectCall {
 
     private operation: any
     private api = new CallApi()
 
     constructor() { }
 
-    async typeOfOperation(params) {
-        if (params.type == ("CARGA")) {
-            return await this.carga(params)
-        } else {
-            return await this.relatory(params)
-        }
-    }
-
     async carga(params) {
-        this.operation = "java -jar " + UTIL.path +
-            params.type.toLowerCase() + " -" + params.crud.substr(0, 1).toLowerCase() + " -t " + params.operation.toUpperCase() + " " + params.path
+        this.operation = `java -jar ${UTIL.pathApiJava} carga -${params.type.substr(0, 1).toLowerCase()} -t ${params.operation.toUpperCase()} ${UTIL.pathCrud}`
 
-        const resultCall = await this.api.call(this.operation)
-        return resultCall
+        const resultCall = await this.api.wrapper(this.operation)
+        return resultCall + this.operation
     }
 
     async relatory(params) {
-        if (params.type == "ESTATISTICA") {
-            this.operation = "java -jar " + UTIL.path +
-                params.type.toLowerCase() + " -pa " + params.period + " -t " + params.operation.toUpperCase() + " " + params.pathSave
+        if (params.operation == "ESTATISTICA") {
+            this.operation = `java -jar ${UTIL.pathApiJava} relatorio -pa ${params.period} -t ${params.operation.toUpperCase()} ${UTIL.pathSave}`
         } else {
-            this.operation = "java -jar " + UTIL.path +
-                params.type.toLowerCase() + " -t " + params.operation.toUpperCase() + " " + params.pathSave
+            this.operation = `java-jar ${UTIL.pathApiJava} relatorio -t ${params.operation.toUpperCase()} ${UTIL.pathSave}`
         }
 
-        const resultCall = await this.api.call(this.operation);
+        const resultCall = await this.api.wrapper(this.operation);
         return resultCall
     }
 }
--- src/wrapper/wrapper.routes.ts
+++ src/wrapper/wrapper.routes.ts
@@ -1,16 +1,94 @@
+import { WrapperService } from './../service/wrapper.service';
 import { Request, Response } from "express";
-import * as redirect from "../redirect";
+import * as _ from 'lodash'
+const del = require('del');
+const csv = require('csvtojson')
+const wrapperService = new WrapperService()
 
-export let insertProduct = (req: Request, res: Response) => {
-  const obj = {
+export let callApiJava = (req: Request, res: Response) => {
+  const params = {
     operation: "PRODUTO",
-    path: "upload/*.csv",
-    type: "CARGA",
+    type: "RELATORIO",
     crud: "INSERT",
-    period: 0,
-    pathSave: "src/service/csvFiles/download/saida.csv"
+    period: 0
   }
 
-  const chamada = redirect.Redirect(obj)
-  res.send(chamada)
+  wrapperService.crudApi(params)
+    .then((resolve) => {
+      res.send(resolve)
+    }).catch((reject) => {
+      console.log(reject)
+    })
+}
+
+export let downloadFile = (req: Request, res: Response) => {
+  const operation = req.params;
+
+  wrapperService.fileDownload(operation)
+    .then((resolve) => {
+      res.download("/home/daniel/Documentos/faculdade/web/projetoMaria-frontend-3fase/src/download/teste.csv")
+      setTimeout(() => {
+        del.sync(['/home/daniel/Documentos/faculdade/web/projetoMaria-frontend-3fase/src/download/data.csv']);
+      }, 1000)
+    }).catch((reject) => {
+      console.log(reject)
+    })
+}
+
+export let loaderFile = (req: Request, res: Response) => {
+  const operation = req.params;
+  const csvFilePath = '/home/daniel/Documentos/faculdade/web/projetoMaria-frontend-3fase/src/download/teste.csv'
+
+  wrapperService.fileDownload(operation)
+    .then((resolve) => {
+      csv()
+        .fromFile(csvFilePath)
+        .then((jsonObj) => {
+          res.send(jsonObj)
+          setTimeout(() => {
+            del.sync(['/home/daniel/Documentos/faculdade/web/projetoMaria-frontend-3fase/src/download/data.csv']);
+          }, 1000)
+        })
+    }).catch((reject) => {
+      console.error(reject)
+    })
+}
+
+export let deleteInfo = (req: Request, res: Response) => {
+  const operation: any = {}
+  operation.operation = req.params.operation;
+  operation.type = "delete"
+
+  wrapperService.crudApi(operation)
+    .then((resolve) => {
+      res.send(resolve)
+    }).catch((reject) => {
+      console.error(reject)
+    })
+}
+
+export let updateInfo = (req: Request, res: Response) => {
+  const operation: any = {}
+  operation.operation = req.params.operation;
+  operation.type = "update"
+
+  wrapperService.crudApi(operation)
+    .then((resolve) => {
+      res.send(resolve)
+    }).catch((reject) => {
+      console.error(reject)
+    })
+}
+
+export let insertInfo = (req: Request, res: Response) => {
+  const operation: any = {}
+  operation.operation = req.params.operation;
+  operation.type = "insert"
+
+  wrapperService.crudApi(operation)
+    .then((resolve) => {
+      res.send(resolve)
+    }).catch((reject) => {
+      console.error(reject)
+    })
 }
