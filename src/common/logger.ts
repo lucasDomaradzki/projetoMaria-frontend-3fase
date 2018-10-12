2cf9da1dcc8a268b2202eaea1e3866491489709e
lucasDomaradzki <lucas_domaradzki@estudante.sc.senai.br>
2018-10-12 20:13:03 -0300

    Adicionada mecanismo de log e validacao do arquivo de upload do front

---
+++ src/common/logger.ts
@@ -0,0 +1,28 @@
+import * as log from "winston";
+import { createLogger } from "winston";
+import { MARIA_UTIL } from "../common/util";
+
+export const logTypes = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2
+  }
+};
+
+const filename = new Date().getDay() + "-" + new Date().getMonth() + "-" + new Date().getFullYear();
+
+export const Logger = createLogger({
+  levels: logTypes.levels,
+  format: log.format.combine(
+    log.format.timestamp({
+      format: 'DD-MM-YYYY hh:mm'
+    }),
+    log.format.json()
+  ),
+  transports: [
+    new log.transports.Console( { stderrLevels: ["error", "warn", "info"] } ),
+    new log.transports.File( { filename: `${MARIA_UTIL.logpath}/BACKEND-${filename}.log` } )
+  ],
+  exitOnError: true
+});
